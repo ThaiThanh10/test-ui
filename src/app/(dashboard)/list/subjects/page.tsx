ddf44f407723889113056f265976a19f8cfac8aa
ThaiThanh10 <116795003+ThaiThanh10@users.noreply.github.com>
2024-10-14 23:22:26 +0700

    first commit

---
+++ src/app/(dashboard)/list/subjects/page.tsx
@@ -0,0 +1,123 @@
+import FormModal from "@/components/FormModal"
+import Pagination from "@/components/Pagination"
+import Table from "@/components/Table"
+import TableSearch from "@/components/TableSearch"
+import { role, subjectsData } from "@/lib/data"
+import Image from "next/image"
+
+type Subject = {
+    id: number
+    name: string
+    khoa: string
+    period: string
+    goal: string
+    result: string
+}
+
+const columns = [
+    {
+        header: "Chỉ số (Đơn vị tính)",
+        accessor: "name",
+    },
+    {
+        header: "Khoa (Phòng)",
+        accessor: "teachers",
+        className: "hidden md:table-cell",
+    },
+    {
+        header: "Chu kỳ đo lường	",
+        accessor: "action",
+    },
+    {
+        header: "Chỉ tiêu ",
+        accessor: "action",
+    },
+    {
+        header: "Kết quả ",
+        accessor: "action",
+    },
+    {
+        header: "Hanh dong",
+        accessor: "action",
+    },
+]
+
+const SubjectListPage = () => {
+    const renderRow = (item: Subject) => (
+        <tr
+            key={item.id}
+            className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-lamaPurpleLight"
+        >
+            <td className="flex items-center gap-4 p-4">
+                {item.id}. {item.name}
+            </td>
+            <td className="hidden md:table-cell">{item.khoa}</td>
+            <td className="hidden md:table-cell">{item.period}</td>
+            <td className="hidden md:table-cell">{item.goal}</td>
+            <td className="hidden md:table-cell">{item.result}</td>
+            <td>
+                <div className="flex items-center gap-2">
+                    {role === "admin" && (
+                        <>
+                            <FormModal
+                                table="subject"
+                                type="update"
+                                data={item}
+                            />
+                        </>
+                    )}
+                </div>
+            </td>
+        </tr>
+    )
+
+    return (
+        <div className="bg-white p-4 rounded-md flex-1 m-4 mt-0">
+            {/* TOP */}
+            {/* <div className="flex items-center justify-between">
+                <h1 className="hidden md:block text-lg font-semibold">All Subjects</h1>
+                <div className="flex flex-col md:flex-row items-center gap-4 w-full md:w-auto">
+                    <TableSearch />
+                    <div className="flex items-center gap-4 self-end">
+                        <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow">
+                            <Image
+                                src="/filter.png"
+                                alt=""
+                                width={14}
+                                height={14}
+                            />
+                        </button>
+                        <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow">
+                            <Image
+                                src="/sort.png"
+                                alt=""
+                                width={14}
+                                height={14}
+                            />
+                        </button>
+                        {role === "admin" && (
+                            <FormModal
+                                table="teacher"
+                                type="create"
+                            />
+                        )}
+                    </div>
+                </div>
+            </div> */}
+            {/* LIST */}
+            <h1 className="hidden md:block text-lg font-semibold">
+                Bao Cao cac chi so chat luong benh vien
+            </h1>
+
+            <Table
+                columns={columns}
+                renderRow={renderRow}
+                data={subjectsData}
+            />
+            {/* PAGINATION */}
+            {/* <Pagination /> */}
+        </div>
+    )
+}
+
+export default SubjectListPage
